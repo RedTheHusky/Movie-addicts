502929f3490864a45199adbb9a7fe1d86f465bb3
RedTheHusky <szepalberttamas@yahoo.com>
2018-12-21 16:55:46 +0200

    Update 1

--- workers/w_home.js
+++ workers/w_home.js
@@ -1,3 +1,22 @@
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/script/main.js'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+
 let timerForCheckingModificationl={milliseconds:'900000',obj:''}; //this is 15 minutes
 let movie4MainItems=[];
 let movie4MainPagination={};
